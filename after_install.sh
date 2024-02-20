1cbe66a75ce5002150347a4c21a83d2559bcb088
Subhadip Maity <127953326+subhadipmaity43@users.noreply.github.com>
2024-02-20 16:43:41 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 docker pull subhadipmaity123/docker-python-app1:latest
-docker stop myCon || true
-docker rm myCon || true
 docker run -d --image subhadipmaity123/docker-python-app1:latest -p 9000:8080
